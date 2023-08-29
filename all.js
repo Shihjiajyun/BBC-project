47d8b4f062f65114ef2c2c417ca79aff92e9c395
shihjiajyun <shihjiajyun@gmail.com>
2023-08-29 13:21:30 +0800

    debug

--- all.js
+++ all.js
@@ -1,28 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById("toggleButton");
-    const hiddenBlock = document.getElementById("hiddenBlock");
-    const scrollContainer = document.getElementById("scrollContainer");
-    let isHiddenBlockVisible = false;
+// document.addEventListener("DOMContentLoaded", function () {
+//     const toggleButton = document.getElementById("toggleButton");
+//     const hiddenBlock = document.getElementById("hiddenBlock");
+//     const scrollContainer = document.getElementById("scrollContainer");
+//     let isHiddenBlockVisible = false;
 
 
-    toggleButton.addEventListener("click", function () {
-        isHiddenBlockVisible = !isHiddenBlockVisible;
-        hiddenBlock.style.display = isHiddenBlockVisible ? "block" : "none";
+//     toggleButton.addEventListener("click", function () {
+//         isHiddenBlockVisible = !isHiddenBlockVisible;
+//         hiddenBlock.style.display = isHiddenBlockVisible ? "block" : "none";
 
-        // 更改 scrollContainer 的寬度
-        if (isHiddenBlockVisible) {
-            scrollContainer.style.width = "calc(100vw - 140px)";
-            // console.log("1");
-        } else {
-            scrollContainer.style.width = "calc(100vw - 50px)";
-            // console.log("2");
-        }
-    });
-    if (!isHiddenBlockVisible) {
-        scrollContainer.style.width = "calc(100vw - 50px)";
-    }
-});
+//         if (isHiddenBlockVisible) {
+//             scrollContainer.style.width = "calc(100vw - 140px)";
+//             // console.log("1");
+//         } else {
+//             scrollContainer.style.width = "calc(100vw - 50px)";
+//             console.log("2");
+//         }
+//     });
+//     if (!isHiddenBlockVisible) {
+//         scrollContainer.style.width = "calc(100vw - 50px)";
+//     }
+// });
+
 window.addEventListener('DOMContentLoaded', event => {
+
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -34,11 +35,15 @@ window.addEventListener('DOMContentLoaded', event => {
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
+
     };
+
     // Shrink the navbar 
     navbarShrink();
+
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
+
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
@@ -47,6 +52,7 @@ window.addEventListener('DOMContentLoaded', event => {
             rootMargin: '0px 0px -40%',
         });
     };
+
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
@@ -56,12 +62,16 @@ window.addEventListener('DOMContentLoaded', event => {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
+                return;
             }
         });
     });
+
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
         elements: '#portfolio a.portfolio-box'
     });
+
 });
+
 
